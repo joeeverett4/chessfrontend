be4c301bbde4bb32681032ec8690693a77cd601e
joeeverett4 <joe.everett34@gmail.com>
2023-08-21 20:16:52 +0100

    frontend mostly working

--- src/components/ChessAi.vue
+++ src/components/ChessAi.vue
@@ -8,59 +8,74 @@
       <!-- WHite player -->
       <!-- Black player -->
       <p>{{ names[outerIndex]?.white }} vs {{ names[outerIndex]?.black }}</p>
-    
-      <div class = "chess-container">
-      <chessboard :fen="positions[outerIndex]" />
 
-      <div class="controls">
+      <div class="chess-container">
+        <chessboard :fen="positions[outerIndex]" />
+
+        <div class="controls">
           <!-- Previous button -->
-      <svg
-        @click="goToMove(-1, outerIndex)"
-        :class="{ disabled: count === 0 }"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <polyline points="15 18 9 12 15 6"></polyline>
-      </svg>
-      
-      <!-- Next button -->
-      <svg
-        @click="goToMove(1, outerIndex)"
-        :class="{ disabled: count === pgns[outerIndex].split(' ').length - 1 }"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <polyline points="9 18 15 12 9 6"></polyline>
-      </svg>
-      </div>
-     <div class = "move-container"> 
-     <div class = "move-list"> 
-      <div style = "margin-bottom:10px;" v-for="i in outerArray[0]" :key="i" >
-         {{i}} 
-        </div>  
-     </div>  
-      <div class="move-grid">
-        <div v-for="(innerObject, innerIndex) in outerArray.slice(1)" :key="innerIndex">
-          <p
-            :style="getParagraphStyle(outerArray[innerIndex + 1]?.next_mistake)"
-            @click="goToMoveOnBoard(outerIndex, innerIndex)"
+          <!-- Previous button -->
+          <svg
+            @click="goToMove(-1, outerIndex)"
+            :class="{ disabled: counts[outerIndex] === 0 }"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           >
-            {{ innerObject.move }}
-          </p>
+            <polyline points="15 18 9 12 15 6"></polyline>
+          </svg>
+
+          <!-- Next button -->
+          <svg
+            @click="goToMove(1, outerIndex)"
+            :class="{
+              disabled:
+                counts[outerIndex] === pgns[outerIndex].split(' ').length - 1,
+            }"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <polyline points="9 18 15 12 9 6"></polyline>
+          </svg>
+        </div>
+        <div class="move-container">
+          <div class="move-list">
+            <div
+              style="margin-bottom: 10px"
+              v-for="i in outerArray[0]"
+              :key="i"
+            >
+              {{ i }}
+            </div>
+          </div>
+          <div class="move-grid">
+            <div
+              v-for="(innerObject, innerIndex) in outerArray.slice(1)"
+              :key="innerIndex"
+            >
+              <p
+                :style="
+                  getParagraphStyle(outerArray.slice(1)[innerIndex + 1]?.next_mistake)
+                "
+                @click="goToMoveOnBoard(outerIndex, innerIndex)"
+              >
+                {{ innerObject.move }}
+                <span style = "font-size:10px;">{{innerObject.evaluation}}</span>
+                
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-     </div> 
-    </div>
     </div>
   </div>
 </template>
@@ -80,7 +95,7 @@ export default {
       nestedArray: [], // This will hold your API data
       pgns: [],
       positions: [],
-      count: 0,
+      counts: [],
       names: [],
     };
   },
@@ -100,34 +115,41 @@ export default {
       }
     },
 
-    goToMoveOnBoard(chessboardIndex, moveIndex) {
-      // Update the count and position to reflect the selected move
-      this.count = moveIndex;
-      this.positions[chessboardIndex] = this.getPositionForMove(
-        this.count,
-        chessboardIndex
-      );
-    },
+   goToMoveOnBoard(chessboardIndex, moveIndex) {
+  // Update the count and position to reflect the selected move
+  console.log("goToMoveOnBoad");
+  this.$set(this.counts, chessboardIndex, moveIndex); // Update the current count
+  this.$set(this.positions, chessboardIndex, this.getPositionForMove(
+    this.counts[chessboardIndex],
+    chessboardIndex
+  ));
+},
 
     goToMove(step, chessboardIndex) {
-      if (
-        this.count + step >= 0 &&
-        this.count + step < this.pgns[chessboardIndex].split(" ").length
-      ) {
-        this.count += step;
-        this.positions[chessboardIndex] = this.getPositionForMove(
-          this.count,
-          chessboardIndex
-        );
-      }
-    },
+  console.log("goToMove");
+  if (
+    this.counts[chessboardIndex] + step >= 0 &&
+    this.counts[chessboardIndex] + step <
+      this.pgns[chessboardIndex].split(" ").length
+  ) {
+    this.$set(this.counts, chessboardIndex, this.counts[chessboardIndex] + step); // Update the count
+    this.$set(this.positions, chessboardIndex, this.getPositionForMove(
+      this.counts[chessboardIndex],
+      chessboardIndex
+    ));
+  }
+},
 
     getPositionForMove(moveIndex, chessboardIndex) {
       let position = new Chess();
       let moves = this.pgns[chessboardIndex].split(" ");
+      console.log(this.positions)
       for (let i = 0; i <= moveIndex; i++) {
+          console.log("inside for loop")
         position.move(moves[i]);
       }
+      console.log(position.fen())
+      // correct fen is being returned
       return position.fen();
     },
 
@@ -161,6 +183,7 @@ export default {
           console.log(this.names);
 
           this.positions = new Array(data.length).fill("start");
+          this.counts = new Array(data.length).fill(0);
         })
         .catch((error) => {
           console.error("Error fetching games:", error);
@@ -176,43 +199,41 @@ export default {
   display: grid;
   grid-template-columns: repeat(2, 1fr); /* Create two equally sized columns */
   gap: 10px; /* Add some space between the moves */
-  margin-left:10px;
-  
+  margin-left: 10px;
 }
 
-.move-container{
-    display:flex;
-    max-height: 400px;
-    overflow: scroll;
-    padding-right: 20px;
-
+.move-container {
+  display: flex;
+  max-height: 400px;
+  overflow: scroll;
+  padding-right: 20px;
 }
-.move-list{
- margin-left:20px;
+.move-list {
+  margin-left: 20px;
 }
 .d-flex {
   margin: 0 auto;
   max-width: 1000px;
   padding-bottom: 50px;
-  position:relative;
+  position: relative;
 }
-.d-flex p{
-    text-align: left;
+.d-flex p {
+  text-align: left;
 }
-.chess-container{
+.chess-container {
   display: flex;
   margin: 0 auto;
   max-width: 1000px;
   position: relative;
 }
-.controls{
-    display: flex;
-    position:absolute;
-    bottom:6%;
-    left:8%;
+.controls {
+  display: flex;
+  position: absolute;
+  bottom: 6%;
+  left: 8%;
 }
-.controls svg{
-    width:40px;
+.controls svg {
+  width: 40px;
 }
 p {
   margin-block-start: 0;
@@ -222,5 +243,4 @@ p {
   opacity: 0.5; /* Add a bit of transparency for disabled buttons */
   cursor: not-allowed; /* Change cursor to indicate non-interactivity */
 }
-
 </style>
